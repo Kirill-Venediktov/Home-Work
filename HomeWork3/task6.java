2d7ac833645076938eb3a074b32f3deaf32b6ab2
Patriarh-Kirill <Venediktovkirill92@gmail.com>
2019-03-15 11:38:18 +0300

    Исправил 6 задачу, все равно не работает

--- HomeWork3/task6.java
+++ HomeWork3/task6.java
@@ -11,12 +11,15 @@ public class task6 {
         System.out.println("Введите исходную строку: ");
 //        String a = in.nextLine();
         StringBuilder stringBuilder;
-        stringBuilder = new StringBuilder(in.next());
+        stringBuilder = new StringBuilder(in.nextLine());
 
-        for (int i = 0; i < stringBuilder.length() - 1; i++) {
+        for (int i = 0; i < stringBuilder.length(); i++) {
             char a = stringBuilder.charAt(i);
             for (int q = 0; q < stringBuilder.length(); q++) {
                 if (a == stringBuilder.charAt(q)) {
+                    stringBuilder.deleteCharAt(q);
+                }
+                if (stringBuilder.charAt(q) == ' ') {
                     stringBuilder.deleteCharAt(q);
                 }
             }
